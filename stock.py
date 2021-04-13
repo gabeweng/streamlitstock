590e7efd2e5e56f0e788f2cd2528ec3401affb2d
Gabriel Weng <gabeweng@gmail.com>
2021-04-13 11:43:53 -0400

    added multiselect :P

--- stock.py
+++ stock.py
@@ -1,32 +1,36 @@
 import yfinance as yf
 import streamlit as st
+
 import datetime
+import pandas as pd
 
 st.write("""
 # Simple Sh*t Poopy Ass
 Shown are the stock **closing price** and ***volume*** of Google!
 """)
-
+Snp500=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+tickers=st.multiselect('Multiselect', Snp500['Symbol'])
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 
-tickerSymbol = st.text_input('Area for textual entry')
-
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
 
 startdate = st.date_input(label='start input',value=(datetime.date(2019,7,6)))
 enddate = st.date_input(label='end input',value=(datetime.date.today()))
+firstable=True
+haveResult=False
+for s in tickers:
 
-tickerDf = tickerData.history(period='1d', start=startdate, end=enddate)
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
-st.write("""
-## Closing Price
-""")
-st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
+    tickerDf = yf.Ticker(s).history(period='1d', start=startdate, end=enddate)
+    # Open	High	Low	Close	Volume	Dividends	Stock Splits
+    if firstable==True:
+        outputdf = tickerDf[['Close']].rename(columns={'Close': s})
+        outputdff = tickerDf[['Volume']].rename(columns={'Volume': s})
+        firstable=False
+    else:
+        outputdf[s]=tickerDf['Close']
+        outputdff[s]=tickerDf['Volume']
+    haveResult=True
+if haveResult:
+    st.line_chart(outputdf)
+    st.line_chart(outputdff)
 st.balloons()
